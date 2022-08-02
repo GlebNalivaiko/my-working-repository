08659000ee7384d871fcaeaa47f8e6b49bd78baa
GlebNalivaiko <110242308+GlebNalivaiko@users.noreply.github.com>
2022-08-02 19:45:45 +0300

    made the second program on java

---
+++ src/Numbers.java
@@ -0,0 +1,12 @@
+public class Numbers {
+    public static void main(String[] args) {
+        int myInt = 1000000;
+    byte myByte = 100;
+    System.out.println(++myInt);
+    double myDouble = 10.5;
+    float myFloat =20.5f;
+    System.out.println(myDouble + myFloat + myByte);
+    System.out.println(myDouble);
+    }
+
+}
